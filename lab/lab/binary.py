7a66f57b2daa2db7cdd426050ac023c2ec9403c4
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-09-18 03:38:08 +0300

    Added simple iteration method

--- lab/lab/binary.py
+++ lab/lab/binary.py
@@ -3,7 +3,7 @@ import math
 import sympy as sp
 
 
-def is_zero(num, approximation=0.00001):
+def is_zero(num, approximation=0.0000001):
     return abs(num) < approximation
 
 
@@ -59,3 +59,76 @@ def dichotomy_iter(function, a, b, epsilon):
             b = x
 
     return (a + b) << 1
+
+
+def derivative_min(func, variable, a, b):
+    x = sp.symbols(variable)
+
+    derivative = sp.diff(func, x)
+
+    critical_points = sp.solve(derivative, x)
+    critical_points = [p.evalf() for p in critical_points if a <= p <= b]
+
+    values = [derivative.subs(x, a).evalf(), derivative.subs(x, b).evalf()] + [derivative.subs(x, p) for p in
+                                                                               critical_points]
+
+    min_value = min(values)
+
+    return min_value
+
+
+def derivative_max(func, variable, a, b):
+    x = sp.symbols(variable)
+
+    derivative = sp.diff(func, x)
+
+    critical_points = sp.solve(derivative, x)
+    critical_points = [p.evalf() for p in critical_points if a <= p <= b]
+
+    values = [derivative.subs(x, a).evalf(), derivative.subs(x, b).evalf()] + [derivative.subs(x, p) for p in
+                                                                               critical_points]
+    max_value = max(values)
+
+    return max_value
+
+
+def iteration_apriory(function, a, b, tau, dmin, dmax, epsilon):
+    z = b - a
+
+    q = (dmax - dmin) / (dmax + dmin)
+
+    num_iterations = math.floor(math.log(abs(z) / epsilon) / math.log(1/q)) + 1
+
+    x = a
+    xs = sp.symbols('x')
+
+    derivative = sp.diff(function, x)
+
+    for _ in range(num_iterations):
+       x = x + sp.sign(derivative.subs(x, xs)) * tau * function.subs(xs, x)
+    return x
+
+
+def iteration_iter(function, a, b, tau, epsilon):
+    x = a
+    xn = b
+    xs = sp.symbols('x')
+
+    derivative = sp.diff(function, x)
+
+    while abs(xn - x) > epsilon:
+        x = xn
+        xn = x + sp.sign(derivative.subs(x, xs)) * tau * function.subs(xs, x)
+    return x
+
+
+def iteration(function, a, b, epsilon, a_priory = False):
+    derivMin = derivative_min(function, 'x', a, b)
+    derivMax = derivative_max(function, 'x', a, b)
+
+    tau = 2 / (derivMin + derivMax)
+
+    if a_priory:
+        return iteration_apriory(function, a, b, derivMin, derivMax, tau, epsilon)
+    else:
+        return iteration_iter(function, a, b, tau, epsilon)
