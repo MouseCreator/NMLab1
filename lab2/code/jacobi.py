796af8eebe277d335143c6dbbc48bba9a2cc08be
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-16 02:22:49 +0300

    added jacobi method

---
+++ lab2/code/jacobi.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+def swap_rows(matrix, row1, row2):
+    matrix[[row1, row2]] = matrix[[row2, row1]]
+    return matrix
+
+
+def find_and_swap(matrix, target, eps):
+    n = matrix.shape[0]
+    for j in range(n):
+        if j == target:
+            continue
+        if np.abs(matrix[target][j]) > eps and np.abs(matrix[j][target] > eps):
+            return swap_rows(matrix, j, target)
+    raise "Unable to create a matrix with non-zero diagonal"
+
+
+def to_non_zero_diagonal(matrix, eps):
+    diagonal_elements = np.diag(matrix)
+    n = diagonal_elements.shape[0]
+    for i in range(n):
+        if np.abs(diagonal_elements[i]) < eps:
+            matrix = find_and_swap(matrix, i, eps)
+
+    return matrix
+
+
+def jacobi(matrix, b, x0, eps):
+    matrix = to_non_zero_diagonal(matrix, eps)
+    n = matrix.shape[0]
+    x_prev = x0
+    x_cur = np.zeros(n)
+    while True:
+        for i in range(n):
+            el_sum = 0
+            for j in range(n):
+                if j == i:
+                    continue
+                el_sum += matrix[i][j] * x_prev[j]
+            x_cur[i] = (b[i] - el_sum) / matrix[i][i]
+        difference = x_cur - x_prev
+        difference_norm = np.linalg.norm(difference)
+        if difference_norm < eps:
+            return x_cur
+        x_prev = x_cur.copy()
+
+
+mtx = np.array([[3, -1, 1],
+                [-1, 2, 0.5],
+                [1, 0.5, 3]])
+b = np.array([1, 1.75, 2.5])
+x = jacobi(mtx, b, np.array([0, 0, 0]), 0.001)
+print(x)
