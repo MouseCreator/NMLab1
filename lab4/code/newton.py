303037d6c2ee021bc8fe1f73409138114f206bd5
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-30 10:06:49 +0200

    added solvable test

--- lab4/code/newton.py
+++ lab4/code/newton.py
@@ -16,10 +16,10 @@ def calculate_function(functions, x_curr):
     return results
 
 
-def initial_solution(variables):
+def initial_solution(variables, val=0.0):
     solution_map = {}
     for v in variables:
-        solution_map[v] = 0.25
+        solution_map[v] = val
     return solution_map
 
 
@@ -33,7 +33,7 @@ def update_solution(x_curr, z):
 
 
 def newton_calc(functions, variables, jacobian, eps):
-    x_init = initial_solution(variables)
+    x_init = initial_solution(variables, 0.25)
     x_curr = x_init
     while True:
         a_matrix = np.array(jacobian.subs(x_curr)).astype(float)
@@ -48,16 +48,36 @@ def newton_calc(functions, variables, jacobian, eps):
 def newton(functions, variables, eps):
     jacobian = find_Jacobian(functions, variables)
     return newton_calc(functions, variables, jacobian, eps)
+def max_second_derivative(functions, x_init):
+    max_val = 0
+    for f in functions:
+        for v in x_init:
+            f_prime = sp.diff(f, v)
+            f_double_prime = sp.diff(f_prime, v)
+            mx = np.abs(f_double_prime.subs(x_init))
+            if mx > max_val:
+                max_val = mx
+    return max_val
 
 
-def test_newton(eps):
-    x, y = sp.symbols('x y')
-    f1 = x ** 2 - 2 * x * y + 1
-    f2 = x ** 2 + y ** 2 - 2
-    funcs = [f1, f2]
-    vars = [x, y]
-    solution = newton(funcs, vars, eps)
-    print(solution)
+def max_f(functions, x_init):
+    max_val = 0
+    for f in functions:
+        mx = np.abs(f.subs(x_init))
+        if mx > max_val:
+            max_val = mx
+    return max_val
+
+
+def test_solvable(functions, jacobian, x_init, eps):
+    A = np.array(jacobian.subs(x_init)).astype(float)
+    n = len(functions)
+    if np.abs(np.linalg.det(A)) < eps:
+        return False
+    L = max_second_derivative(functions, x_init)
+    M = np.linalg.norm(np.linalg.inv(A), ord=2)
+    D = max_f(functions, x_init)
+    return np.abs(M * M * L * D * n * n - 1/2) < eps
 
 
 def create_function(variables, n, i):
@@ -81,5 +101,25 @@ def test_newton_n_space(n, eps):
     solution = newton(functions, variables, eps)
     print(solution)
 
+def test_newton(eps):
+    x, y = sp.symbols('x y')
+    f1 = x ** 2 - 2 * x * y + 1
+    f2 = x ** 2 + y ** 2 - 2
+    funcs = [f1, f2]
+    vars = [x, y]
+    solution = newton(funcs, vars, eps)
+    print(solution)
 
-test_newton_n_space(10, 1e-5)
+def is_solvable():
+    x, y = sp.symbols('x y')
+    f1 = x - 0.5 * sp.sin((x-y)/2)
+    f2 = y - 0.5 * sp.cos((x+y)/2)
+    funcs = [f1, f2]
+    vars = [x, y]
+    J = find_Jacobian(funcs, vars)
+    x_init = initial_solution(vars, 0)
+    print("Solvable?", test_solvable(funcs, J, x_init, 1e-5))
+
+
+is_solvable()
+
