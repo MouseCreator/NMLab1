24b31d86224dcf5eb2a1549f4b0856802bfa6cd3
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-12 17:01:36 +0200

    added lagrange test

--- lab5/code/lagrange.py
+++ lab5/code/lagrange.py
@@ -1,6 +1,5 @@
 import val as v
 import sympy as sp
-import numpy as np
 
 
 def at(expression, x_value):
@@ -9,14 +8,18 @@ def at(expression, x_value):
     return expression.subs(x_map_t)
 
 
+def simplify(expr):
+    return sp.simplify(expr)
+
+
 def lagrange_interpolation(vals):
     x = sp.symbols('x')
     v_prod = v_product(vals, x)
     v_der = derivative_v_product(v_prod, x)
     polynomial = 0
     for val in vals:
-        polynomial = polynomial + (v_prod * val.function()) / (x - val.argument() * at(v_der, val.argument()))
-    return polynomial
+        polynomial = polynomial + (v_prod * val.function()) / ((x - val.argument()) * at(v_der, val.argument()))
+    return simplify(polynomial)
 
 
 def v_product(vals, x):
@@ -25,9 +28,17 @@ def v_product(vals, x):
     else:
         expression = 1
         for val in vals:
-            expression *= (x - val)
+            expression *= (x - val.argument())
         return expression
 
 
 def derivative_v_product(expression, x):
     return sp.diff(expression, x)
+
+
+v1 = v.Val([-1, 1 / 3])
+v2 = v.Val([0, 1])
+v3 = v.Val([1, 3])
+
+p = lagrange_interpolation([v1, v2, v3])
+print(p)
