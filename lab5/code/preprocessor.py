562a965a0df9284311b7813df86f99fea469d6c3
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-12 18:24:01 +0200

    added values with derivatives

--- lab5/code/preprocessor.py
+++ lab5/code/preprocessor.py
@@ -1,8 +1,16 @@
 import numpy as np
 import val as v
+import sympy as sp
 
 
-def optimal_values(function, a, b, n):
+def nth_derivative_at_x0(expr_str, n, x0):
+    x = sp.symbols('x')
+    expr = sp.sympify(expr_str)
+    nth_derivative = sp.diff(expr, x, n)
+    return nth_derivative.subs(x, x0)
+
+
+def optimal_values(function, a, b, n, rank=0):
     if a > b:
         raise ValueError("Illegal range!")
     vals_arr = []
@@ -10,24 +18,29 @@ def optimal_values(function, a, b, n):
     ab_diff = (b - a) / 2
     for i in range(n):
         x = ab_sum + ab_diff * np.cos((2 * i + 1) * np.pi / (2 * (n + 1)))
-        f = v.at(function, x)
-        vals_arr.append(v.Val([x, f]))
+        to_add(function, x, rank)
     return vals_arr
 
 
-def even_values(function, a, b, n):
+def even_values(function, a, b, n, rank=0):
     if a > b:
         raise ValueError("Illegal range!")
     vals_arr = []
     if n == 1:
-        x = (a+b)/2
-        f = v.at(function, x)
-        return [v.Val([x, f])]
+        x = (a + b) / 2
+        to_add(function, x, rank)
     step = (b - a) / (n - 1)
     curr = a
     for i in range(n):
         x = curr
         curr += step
-        f = v.at(function, x)
-        vals_arr.append(v.Val([x, f]))
+        vals_arr.append(to_add(function, x, rank))
     return vals_arr
+
+
+def to_add(function, x, rank):
+    f = v.at(function, x)
+    arr = [x, f]
+    for j in range(rank):
+        arr.append(nth_derivative_at_x0(function, x, j + 1))
+    return v.Val(arr)
